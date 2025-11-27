21dd090319161d82d82d311fe35d54f2c959bfe7
ginsabu <155948685+ginsabu@users.noreply.github.com>
2025-11-27 15:44:11 +0900

    type 이름 출력

--- main.c
+++ main.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-char *proverb = "All that glisters is not gold.";
+#define MAX_NODETYPE 7
+#define MAX_CHARNAME 32
 
-void setPointer(char **q)
+#define SMMNODE_TYPE_LECTURE     0
+#define SMMNODE_TYPE_RESTAURANT  1
+#define SMMNODE_TYPE_LABORATORY  2
+#define SMMNODE_TYPE_HOME        3
+#define SMMNODE_TYPE_GOTOLAB     4
+#define SMMNODE_TYPE_FOODCHANCE  5
+#define SMMNODE_TYPE_FESTIVAL    6
+
+typedef int smmNode_e;
+
+static char smmNodeName[MAX_NODETYPE][MAX_CHARNAME] = {
+    "lecture",
+    "restaurant",
+    "laboratory",
+    "home",
+    "gotoLab",
+    "foodChance",
+    "festival"
+};
+
+char* getName(smmNode_e type)
 {
-    *q = proverb;
+    if (type < 0 || type >= MAX_NODETYPE)
+        return NULL;
+    return smmNodeName[type];
 }
 
 int main(void)
 {
-    char *p = "zzz";
-    setPointer(&p);
-    printf("%s\n", p);
+    smmNode_e t1 = SMMNODE_TYPE_LECTURE;
+    smmNode_e t2 = SMMNODE_TYPE_FOODCHANCE;
+
+    printf("Type name = %s\n", getName(t1));
+    printf("Type name = %s\n", getName(t2));
+
     return 0;
 }
