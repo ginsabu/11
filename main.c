b72a35cb409be0af3bd477fe6879ba00b50b52db
ginsabu <155948685+ginsabu@users.noreply.github.com>
2025-11-27 15:26:09 +0900

    pr4

--- main.c
+++ main.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main(void)
+{
+    int i;
+    int grade[5];
+    int *p;
+    int sum = 0;
 
-void swap (int *x, int *y){
-	
-	int temp;
-	temp = *x;
-	*x = *y;
-	*y = temp;
-	
+    for (i = 0; i < 5; i++)
+    {
+        printf("input value[%d] = ", i);
+        scanf("%d", &grade[i]);
+    }
+
+    p = grade;
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("grade[%d] = %d\n", i, *(p + i));
+        sum += *(p + i);
+    }
+
+    printf("average: %d\n", sum / 5);
+
+    return 0;
 }
 
-void main(void){
-	
-	int a = 3;
-	int b = 5;
-	
-	swap(&a, &b);
-	
-	printf("a: %i, b: %i\n", a, b);
-	
-}
